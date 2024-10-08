ea7f2bf55612f6c5f1d81d53fc62e722ede01890
yinhla <makamuyinhla2016@gmail.com>
2024-10-08 21:57:35 +0200

    Fixed the login page and created routing plan of the application

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Login from "./screens/Login";
 
-import SideBar from "./components/SideBar";
 
-import "./App.css";
-import EventManagement from "./screens/EventManagement.jsx";
-import NavBar from "./components/NavBar.jsx";
-import "./style/EventManagement.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import EventPage from "./screens/EventPage .jsx";
-import RegistrationForm from "./screens/RegistrationForm.jsx";
-import UserDetails from "./screens/UserDetails.jsx";
-import FeedbackReviews from "./screens/FeedbackReviews.jsx";
-import ManagedOrganizers from "./screens/ManagedOrganizers.jsx";
-import CalendarPage from "./screens/CalendarPage.jsx";
-import ContactUs from "./screens/ContactUs.jsx";
+
+
 
 function App() {
   return (
-    <>
-      <ContactUs/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Login/> }/>
+      </Routes>
+    </Router>
   );
 }
 export default App;
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -5,24 +5,31 @@ import axios from 'axios';
 import '../style/style.css'
 
 import { useNavigate } from "react-router-dom";
+
 function Login() {
+
     const navigate = useNavigate();
 
     const [Email, setEmail] = useState('');
     const [Password, setPassword] = useState('');
 
-    const login = () => {
+    const login = async() => {
         var data = { email: Email, password: Password }
-        axios.post('http://localhost:3001/api/login', data).then(respond => {
+        await axios.post('http://localhost:3001/api/admin/login', data).then(respond => {
             console.log(respond);
-            if (respond.data.success) {
-                console.log(respond.data.results[0]);
+            if (respond.status === 200) {
+                localStorage.setItem("token", respond.data.token)
+                localStorage.setItem("type", respond.data.type)
                 var user = {
-                    email: respond.data.results[0].email,
-                    id: respond.data.results[0].id
+                    user_id : respond.data.result[0].admin_id,
+                    username : respond.data.result[0].username,
+                    email : respond.data.result[0].email,
+                    created_at : respond.data.result[0].created_at,
                 }
+                console.log(JSON.stringify(user))
+
                 localStorage.setItem('user_info', JSON.stringify(user));
-                navigate('/emloyee')
+                navigate('/home')
             }
             else {
                 toast.warn(respond.data.message);
@@ -41,7 +48,7 @@ function Login() {
             <input type="email" onChange={(event) => setEmail(event.target.value)} className="control-form" />
         </div>
         <div className="form-group">
-            <label>First name</label>
+            <label>Password</label>
             <input type="password" onChange={(event) => setPassword(event.target.value)} className="control-form" />
         </div>
         <div className="form-group">
