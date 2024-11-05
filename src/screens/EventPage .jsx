2dd85d4f1305072f8db2f1bb830cdae5f1bd02f7
Lethabo-T-Molefe <lethabomolefe69@gmail.com>
2024-11-05 09:30:07 +0200

    modified:   src/screens/EventPage .jsx

--- src/screens/EventPage .jsx
+++ src/screens/EventPage .jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from "react";
+import React, { useState,   }  from "react";
 import "../style/EventPage.css"; // Custom CSS for this page
 import { useNavigate } from "react-router-dom";
 import SideBar from "../components/SideBar";
@@ -91,6 +91,7 @@ const EventPage = () => {
       console.log(error);
     });
   };
+  
   
 
   return (
