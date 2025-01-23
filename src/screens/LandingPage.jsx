ce0eb6ec496638ed83829ca21ba10860853ecb40
Lethabo-T-Molefe <lethabomolefe69@gmail.com>
2025-01-23 08:43:37 +0200

    modified:   src/screens/LandingPage.jsx     imported Link from react-router-dom

--- src/screens/LandingPage.jsx
+++ src/screens/LandingPage.jsx
@@ -6,6 +6,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 import api from '../APIs/API';
+import { Link } from 'react-router-dom';
 
 
 
